24fe1f8b4996fad3be468c1524262cb8cea348a4
kyungsooseyo <850767270@qq.com>
2022-10-23 00:10:54 +0800

    feat:基础查询功能和useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,37 +1,53 @@
-
-import { List } from './list';
-import { SearchPanel } from './search-panel';
+import { List } from './list'
+import { SearchPanel } from './search-panel'
 import React, { useState, useEffect } from 'react'
-import { cleanObject } from 'utils';
-import * as qs from 'qs';
+import { cleanObject, useMount, useDebounce } from 'utils'
+import * as qs from 'qs'
 console.log(qs)
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
-  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([])
+  const [list, setList] = useState([])
   const [param, setParam] = useState({
     name: '',
     personId: '',
-  });
+  })
+  const debounceParam = useDebounce(param, 1000)
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (res) => {
-      console.log('rrrr', res)
-      if (res.ok) {
-        setList(await res.json());
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(
+      async (res) => {
+        console.log('rrrr', res)
+        if (res.ok) {
+          // const a = await res.json()
+          // console.log('🚀 ~ file: index.jsx ~ line 21 ~ fetch ~ json', a)
+          setList(await res.json())
+        }
       }
-    });
-  }, [param]);
-  useEffect(() => {
+    )
+  }, [debounceParam])
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
-        setUsers(await res.json());
+        setUsers(await res.json())
       }
-    });
-  }, [])
+    })
+  })
   return (
-    <div>
-      <SearchPanel param={param} setParam={setParam} users={users}></SearchPanel>
-      <List list={list} users={users}></List>
+    <div
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <div>
+        <SearchPanel
+          param={param}
+          setParam={setParam}
+          users={users}
+        ></SearchPanel>
+      </div>
+      <div style={{ display: 'inline-block' }}>
+        <List list={list} users={users}></List>
+      </div>
     </div>
-  );
-};
+  )
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 //~ 在一个函数里 改变传入的的对象本身是不好的
 export const cleanObject = (object) => {
   const result = { ...object };
@@ -11,3 +13,47 @@ export const cleanObject = (object) => {
 };
 
 export const isFalsy = (value) => (value === 0 ? false : !value);
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+export const useDebounce = (value, delay) => {
+  //? 好像会多次执行呢
+  console.log("vv", value);
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // ~ 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // ~ 每次在上一个useEffect处理完以后再运行
+    return () => {
+      console.log("clear");
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+  return debouncedValue;
+};
+/**
+ *降频函数
+ *这个 debounce 函数在给定的时间间隔内只允许你提供的回调函数执行一次，以此降低它的执行频率。
+ *调用:	debounce(function() {}, 250)
+ * @param {*} func回调函数
+ * @param {*} wait等待时间,推荐250
+ * @param {*} immediate
+ * @returns
+ */
+export const debounce = (func, wait, immediate) => {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
